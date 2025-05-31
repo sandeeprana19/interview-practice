492343764304d50d0f396ba225d09f5fd4e882b8
Sandeep Rana <62502757+sandeeprana19@users.noreply.github.com>
2025-05-31 18:26:48 +0530

    main
    Implement code changes to enhance functionality and improve performance

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,5 +1,30 @@
 import { useState } from "react";
 
+// Progress Bar related question:
+// ==================================================================================
+// Create a Progress Bar Component in React that visually represents a single
+// progress value. Users should be able to increment or decrement progress
+// buttons. Make sure to use inline css.
+// ==================================================================================
+// Requirements:
+// 1. Display one progress bar.
+// 2. The bar should reflect a numeric progress (0—100%).
+// 3. Include buttons labeled '+10%' and '-10%' to increase or decrease the bar's
+// value.
+// 4. Prevent values from going below 0% or above 100%.
+// 5. Change bar color based on value (e.g., red, orange, green).
+// 6. Color is selected based on thresholds:
+// » Red if less than 40%
+// » Orange if between 40-79%
+// » Green if 80% or more
+// ==================================================================================
+// Constraints & Edge Cases:
+// » Constraint 1: Progress must stay between 0 and 100.
+// » Constraint 2: The background color-changing div must have an
+// id="testBgColor" for testing the background color of progress bar.
+// » Edge Case 1: If decrementing would take a value below 0, clamp it to 0.
+// » Edge Case 2: If incrementing would take a value above 100, clamp it to 100.
+
 const ProgressBar = () => {
   const [progress, setProgress] = useState(0);
 
